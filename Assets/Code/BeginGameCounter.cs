28f91f1dacf47325a63d6bbaa15859bf7a4338b5
Maximilian Oettl <oettlm@cip.ifi.lmu.de>
2023-07-17 16:23:49 +0200

    max: start game with delay in 3, 2, 1, start

---
+++ Assets/Code/BeginGameCounter.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class BeginGameCounter : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stateInfo.IsName("three"))
+        {
+            Debug.Log("stateInfo: three");
+            Time.timeScale = 0;
+        } 
+        else if (stateInfo.IsName("two"))
+        {
+            BeginGameText beginGameIn = GameObject.Find("GameObject").GetComponent<BeginGameText>();
+            beginGameIn.SetText("2");
+            Debug.Log("stateInfo: two");
+        }
+        else if (stateInfo.IsName("one"))
+        {
+            BeginGameText beginGameIn = GameObject.Find("GameObject").GetComponent<BeginGameText>();
+            beginGameIn.SetText("1");
+            Debug.Log("stateInfo: one");
+        }
+        else if (stateInfo.IsName("start"))
+        {
+            BeginGameText beginGameIn = GameObject.Find("GameObject").GetComponent<BeginGameText>();
+            beginGameIn.SetText("Start"); 
+            Debug.Log("stateInfo: start");
+            Time.timeScale = 0.4f;
+        }
+        else if (stateInfo.IsName("play"))
+        {
+            BeginGameText beginGameIn = GameObject.Find("GameObject").GetComponent<BeginGameText>();
+            beginGameIn.SetText(string.Empty); 
+            Debug.Log("stateInfo: play");
+            Time.timeScale = 1f;
+        }
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+        
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Assets/Code/BeginGameText.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BeginGameText : MonoBehaviour
+{
+    public TextMeshProUGUI Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetText(string  text)
+    {
+        Text.SetText(text);
+        if (Text != null && text.Equals(string.Empty))
+        {
+            Text.enabled = false;
+        }
+    }
+}
